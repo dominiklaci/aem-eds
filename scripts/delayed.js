f280f0f792bff89a3fa667ad3531718719b7e8d1
michaelberghammer <michael.berghammer1@ibm.com>
2025-06-05 11:25:30 +0200

    Refactor, Adapt hero component

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,4 @@
 // add delayed functionality here
+document.dispatchEvent(new Event('delayed-phase'));
+
+Window.DELAYED_PHASE = true;
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,10 +20,15 @@ import {
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+  const link = main.querySelector('a');
   // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+  if (
+    h1
+    && picture
+    && h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING
+  ) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', { elems: [picture, h1, link] }));
     main.prepend(section);
   }
 }
